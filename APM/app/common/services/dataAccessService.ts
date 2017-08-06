fc704d4c580ef03c64f66d6a0b34acceca6f0f19
runnerdave <runnerdave@gmail.com>
2017-08-06 14:57:07 +1000

    added mock for service calls

---
+++ APM/app/common/services/dataAccessService.ts
@@ -0,0 +1,28 @@
+module app.common {
+    import Product = app.domain.Product;
+
+    interface IDataAccessService {
+        getProductResource(): ng.resource.IResourceClass<IProductResource>;
+    }
+
+    interface IProductResource extends ng.resource.IResource<Product> {
+
+    }
+
+    export class DataAccessService implements IDataAccessService {
+        static $inject = ["$resource"];
+
+        constructor(private $resource: ng.resource.IResourceService) {
+
+        }
+
+        getProductResource(): ng.resource.IResourceClass<IProductResource> {
+            return this.$resource("/api/products/:productId");
+        }
+    }
+
+    angular
+        .module("common.services")
+        .service("dataAccessService",
+            DataAccessService);
+}
--- APM/app/common/services/productResourceMock.ts
+++ APM/app/common/services/productResourceMock.ts
@@ -1,4 +1,3 @@
-/*
 module app.common {
 
     var mockResource = angular
@@ -6,9 +5,10 @@ module app.common {
             ["ngMockE2E"]);
 
     mockResource.run(mockRun);
-    
+
     mockRun.$inject = ["$httpBackend"];
-    function mockRun($httpBackend: ng.IHttpBackendService) : void {
+
+    function mockRun($httpBackend: ng.IHttpBackendService): void {
         var products: app.domain.IProduct[] = [];
         var product: app.domain.IProduct;
 
@@ -26,13 +26,13 @@ module app.common {
             "15-inch steel blade hand saw",
             "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png");
         products.push(product);
- 
-         product = new app.domain.Product(4, "Hammer", "TBX-0048", new Date(2013, 4, 21), 8.99,
+
+        product = new app.domain.Product(4, "Hammer", "TBX-0048", new Date(2013, 4, 21), 8.99,
             "Curved claw steel hammer",
             "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png");
         products.push(product);
-        
-         product = new app.domain.Product(5, "Video Game Controller", "GMG-0042", new Date(2012, 9, 25), 35.95,
+
+        product = new app.domain.Product(5, "Video Game Controller", "GMG-0042", new Date(2012, 9, 25), 35.95,
             "Standard five-button video game controller",
             "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png");
         products.push(product);
@@ -42,8 +42,8 @@ module app.common {
         $httpBackend.whenGET(productUrl).respond(products);
 
         var editingRegex = new RegExp(productUrl + "/[0-9][0-9]*", '');
-        $httpBackend.whenGET(editingRegex).respond(function(method, url, data) {
-            var product = { "productId": 0 };
+        $httpBackend.whenGET(editingRegex).respond(function (method, url, data) {
+            var product = {"productId": 0};
             var parameters = url.split('/');
             var length = parameters.length;
             var id = +parameters[length - 1];
@@ -60,12 +60,11 @@ module app.common {
         });
 
         // Catch all for testing purposes
-        $httpBackend.whenGET(/api/).respond(function(method, url, data) {
+        $httpBackend.whenGET(/api/).respond(function (method, url, data) {
             return [200, products, {}];
         });
-                
+
         // Pass through any requests for application files
         $httpBackend.whenGET(/app/).passThrough();
     }
 }
-*/
